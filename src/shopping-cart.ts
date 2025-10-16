9d0170b354faf53626aaec5eac89bcacbf4011fd
a7med3ouda <ahmed.ouda.official@gmail.com>
2025-10-17 02:13:46 +0300

    add product validation & calculate total price

--- src/shopping-cart.ts
+++ src/shopping-cart.ts
@@ -9,11 +9,20 @@ export class ShoppingCart {
 	}
 
 	addProduct(product: Product, quantity: number): void {
+		if (!product.name) throw new Error("Product name is required");
+		if (product.unitPrice <= 0) throw new Error("Product price must be greater than 0");
+		if (quantity <= 0) throw new Error("Quantity must be greater than 0");
+
 		const existingItem = this.items.find((item) => item.product.name === product.name);
 		if (existingItem) {
 			existingItem.quantity += quantity;
 		} else {
 			this.items.push(new CartItem({ product, quantity }));
 		}
+	}
+
+	getTotalPrice(): number {
+		const total = this.items.reduce((sum, item) => sum + item.product.unitPrice * item.quantity, 0);
+		return Math.round(total * 100) / 100;
 	}
 }
